b3d7547f62d1ad5735bb698ec5e4dea3a54b31ab
AsiburSumon <sumonasif68@gmail.com>
2022-10-12 09:46:47 +0600

    make responsive

--- src/components/Analysis/Analysis.js
+++ src/components/Analysis/Analysis.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import './Analysis.css';
 
 const Analysis = ({quizzes}) => {
@@ -13,13 +13,15 @@ const Analysis = ({quizzes}) => {
     return (
         <div className='rechart'>
             <h3>Topics are analysis by a chart</h3>
-           <div>
-                <LineChart width={500} height={400} data={allQuizzes}>
-                    <Line type="monotype" dataKey="total" stroke="#82ca9d"/>
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip/>
+           <div  className='line-chart'>
+                <ResponsiveContainer width={'99%'} height={400}>
+                    <LineChart width={500}  data={allQuizzes}>
+                        <Line type="monotype" dataKey="total" stroke="#82ca9d"/>
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip/>
                     </LineChart>
+                </ResponsiveContainer>
            </div>
             
         </div>
